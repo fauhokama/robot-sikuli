06e8fd00fde78c75c3da5b0e7fd93bb966b1443a
Faustino Hokama <fhokama@equoplatform.com>
2022-10-10 02:56:08 -0300

    Styling

--- src/robotSikuliEditor.ts
+++ src/robotSikuliEditor.ts
@@ -61,10 +61,6 @@ export class RobotSikuliEditorProvider {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<title>Robot Sikuli</title>
 
-                <link rel="preconnect" href="https://fonts.googleapis.com">
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                <link href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=swap" rel="stylesheet">
-
                 <link href="${styleResetUri}" rel="stylesheet" />
 				<link href="${styleVSCodeUri}" rel="stylesheet" />
 				<link href="${styleMainUri}" rel="stylesheet" />
